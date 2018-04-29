4d07cb1dde09061ea3e833ff5c072e4ad107bb75
Ranran15 <rrui1510@gmail.com>
2018-04-28 22:57:02 -0500

    n-server schema, prototype perdersen commitment

--- tutorial/src/main.cpp
+++ tutorial/src/main.cpp
@@ -8,7 +8,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <vector>
+#include <cmath>
 #include <openssl/aes.h>
+#include <openssl/sha.h>
 
 using namespace libsnark;
 using namespace std;
@@ -17,6 +19,9 @@ const int PLAIN_LENGTH = 140;
 const int TABLE_WIDTH = 1<<9;
 const int TABLE_HEIGHT = 1<<8;
 const int BOX_OVERHEAD = 16;
+
+const int WALLET_LENGTH = 128;
+
 
 //plaintext length plus 2 public keys and 2 message digests
 const int SLOT_LENGTH = PLAIN_LENGTH + 2*(KEYSIZE + BOX_OVERHEAD);
@@ -50,6 +55,12 @@ class ByteArray{
             }
         }
 
+        byte & operator[](size_t i){
+            if(i>=N)
+                throw out_of_range("ByteArray::[] out of range");
+            return data[i];
+        }
+
         static const size_t size = N;
 
         //initialize random ByteArray
@@ -69,6 +80,17 @@ class ByteArray{
 
         byte* getData(){
             return data;
+        }
+
+        size_t getSize(){
+            return N;
+        }
+        long toLong(){
+            return *(long*)data;
+        }
+
+        void setWithLong(long i){
+            memcpy(data, (char*)&i, sizeof(long));
         }
 };
 
@@ -111,10 +133,11 @@ class Prf{
 // }
 
 struct plainQuery{
-    BitMatrixRow row;
+    BitMatrixRow v;
 
     //Array of booleans
-    bool v[TABLE_HEIGHT];
+    //bool b[TABLE_HEIGHT];
+    int b[TABLE_HEIGHT];
 
     //array of random keys
     ByteArray<KEYSIZE> s[TABLE_HEIGHT];
@@ -162,19 +185,46 @@ vector<plainQuery> genShare( location l, string msg, serverInfo si){
         q1.s[i] = ByteArray<KEYSIZE>();
         q1.s[i].random();
 
-        q1.v[i] = (rand()%2 == 0);
+        q1.b[i] = (rand()%2 == 0);
+        //q1.v = 
     }
 
+    ByteArray<KEYSIZE> s_star[TABLE_HEIGHT] = ByteArray<KEYSIZE>(true);
+    //s_star.random();
 
 
 
     plainQuery q2 = q1;
+    //2 server method
     //reset q2's v and s to only differ at row Index , y
-    q2.v[l.y] =  !q1.v[l.y];
-    q2.s[l.y] = ByteArray<KEYSIZE>(true);
+    //q2.b[l.y] =  !q1.b[l.y];
+    //q2.s[l.y] = ByteArray<KEYSIZE>(true);
+
+    //N-server method:
+    //sum of bi = elx
+    //sum of si = s* * elx
+    for(int i=0;i<TABLE_HEIGHT;i++){
+        if(i==l.y){
+            q2.b[i] = 1-q1.b[l.y];
+            q2.s[i].setWithLong(s_star[i].toLong() - q1.s[l.y].toLong());
+        }else{
+            q2.b[i] = 0-q1.b[i];
+            q2.s[i].setWithLong(0 - q1.s[i].toLong());
+            // if(q1.b[i]+q2.b[i]!=0){
+            //     cout<<"something wrong at index "<<i<<endl;
+            //     cout<<"q1.b[i] "<<q1.b[i]<<endl;
+            // }
+            // if(q1.s[i].toLong()+q2.s[i].toLong()!=0){
+            //     cout<<"something wrong at index "<<i<<endl;
+            //     cout<<"q1.s[i] "<<q1.s[i].toLong()<<endl;
+            // }
+            //cout<<"should be 0:"<<(q1.b[i]+q2.b[i]);
+        }
+    }
 
 
-
+    cout<<"l.y: "<<l.y<<endl;
+    cout<<"==========================="<<endl;
     vector<plainQuery> queries;
     queries.push_back(q1);
     queries.push_back(q2);
@@ -232,6 +282,182 @@ void test_r1cs_gg_ppzksnark(size_t num_constraints, size_t input_size)
     assert(bit);
 }
 
+struct wallet
+{
+    char* id = new char[65];
+    int balance;    
+};
+const int CLIENT_ID_LENGTH = WALLET_LENGTH/2;
+//typedef ByteArray<CLIENT_ID_LENGTH> clientId;
+
+
+void sha256(char *string, char outputBuffer[65])
+{
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, string, strlen(string));
+    SHA256_Final(hash, &sha256);
+    int i = 0;
+    for(i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    {
+        sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
+    }
+    outputBuffer[64] = 0;
+}
+//This should use a trusted 3rd party to issue credential
+wallet regWallet(char* id){
+    wallet w;
+    sha256(id,w.id);
+    w.balance=1;
+    return w;
+}
+
+bool verifyWallet(char* hash1, char* hash2){
+    return strcmp(hash1, hash2) == 0;
+}
+//This signing process should be implemented with zk
+//Here is just a simple demo without zk
+bool signWallet(wallet w){
+    char* client_ip = "127.0.0.1";
+
+    char* hash = new char[65];
+    sha256(client_ip,hash);
+
+    bool result = verifyWallet(w.id, hash);
+
+    delete[] hash;
+    //reject or process
+    return result;
+
+}
+
+struct commitments
+{
+    vector<vector<long>> ss;
+    //vector<long> c2;
+    vector<vector<long>> bs;
+
+    //vector<long> c3,c4;
+};
+
+long computeCommitment(long g, long x, long h, long r, long p){
+    return ((long)pow(g,x) * (long)pow(h,r)) % p;
+}
+
+//Generate Pedersen Commitment
+commitments Pedersen(vector<plainQuery> & query){
+    ByteArray<KEYSIZE> s1[TABLE_HEIGHT] = query[0].s;
+    ByteArray<KEYSIZE> s2[TABLE_HEIGHT] = query[1].s;
+
+    auto b1 = query[0].b;
+    auto b2 = query[1].b;
+
+    //primes
+    // long p  = 1301081;
+    // long g,h = 1300051, 1299743;
+    // long r = 1299721;
+
+    long p  = 1109;
+    long q = 277;
+    long g = 966;
+    long r = 75;
+
+    long g1 = 127;
+    long h = 29;
+
+    //TODO: x should loop through s and b
+    //long x = 30;
+    commitments c;
+    vector<long> c1,c2,c3,c4;
+    c.ss.push_back(c1);
+    c.ss.push_back(c2);
+    c.bs.push_back(c3);
+    c.bs.push_back(c4);
+    for(int i=0;i<TABLE_HEIGHT;i++){
+        long x = s1[i].toLong();
+        long x2 = s2[i].toLong();
+
+
+        c.ss[0].push_back(computeCommitment(g,x,h,r,p)); 
+        c.ss[1].push_back(computeCommitment(g,x2,h,r,p));
+
+        if(i==0){
+            cout<<"ss[10]"<<computeCommitment(g,x,h,r,p)<<endl;
+            //cout<<"inputs: "<<g<<" "<<x<<" "<<h<<" "<<r<<" "<<p<<" "<<s1[i]<<endl;
+        }
+
+        long x3 = (long) b1[i];
+        long x4 = (long) b2[i];
+
+        
+        long cc3=computeCommitment(g,x3,h,r,p);
+        long cc4 = computeCommitment(g,x4,h,r,p);
+        if(i==239){
+            cout<<"at i=239: b1+b2="<<x3+x4<<endl;
+            cout<<"cc3+cc4 = "<<cc3+cc4<<endl;
+            cout<<"commitment of 1: "<<computeCommitment(g,1,h,r+r,p)<<endl;
+        }
+        if(i==238){
+            cout<<"at i=238: b1+b2="<<x3+x4<<endl;
+            cout<<"cc3+cc4 = "<<cc3+cc4<<endl;
+            cout<<"commitment of 0: "<<computeCommitment(g,0,h,r+r,p)<<endl;
+        }
+        c.bs[0].push_back(cc3); 
+        c.bs[1].push_back(cc4);
+    }
+
+    //long c = ((long)pow(g,x) * (long)pow(h,r)) % p;
+
+
+
+    //commitment m = g^r * h^m
+    //vector<long>
+    return c;
+}
+
+bool verifyPederson(commitments & cc, plainQuery & query, int server_idx){
+    long p  = 1109;
+    long q = 277;
+    long g = 966;
+    long r = 75;
+
+
+    long g1 = 127;
+    long h = 29;
+    //long x = 30;
+    vector<long> Bsum, Ssum;
+    ByteArray<KEYSIZE> s[TABLE_HEIGHT] = query.s;
+    auto b = query.b;
+    //verify it's correct commitment to server_idx
+    for(size_t i=0;i<TABLE_HEIGHT;i++){
+        long x = s[i].toLong();
+        long x3 = (long) b[i];
+
+        if(cc.ss[server_idx][i]!=computeCommitment(g,x,h,r,p)){
+            //cout<<"inputs: "<<g<<" "<<x<<" "<<h<<" "<<r<<" "<<p<<" "<<s[i]<<endl;
+            cout<<"commitment not equal!!!!!\n"<<"i:"<<i<<";\n values:\n ss[idx][i]:"<<cc.ss[server_idx][i]<<"\ncomputed:"<<computeCommitment(g,x,h,r,p)<<endl;
+            //cout<<"inputs: "<<g<<" "<<x<<" "<<h<<" "<<r<<" "<<p<<" "<<s[i]<<endl;
+            return false;
+        }
+        if(cc.bs[server_idx][i]!=computeCommitment(g,x3,h,r,p)){
+            cout<<"commitment not equal!!!!!\n"<<"i:"<<i<<";\n values:\nbs[idx][i]:"<<cc.bs[server_idx][i]<<"\ncomputed:"<<computeCommitment(g,x,h,r,p)<<endl;
+            return false;
+        }
+
+        Bsum.push_back(cc.ss[0][i]+cc.ss[1][i]);
+        Ssum.push_back(cc.bs[0][i]+cc.bs[1][i]);
+        if(i==239)
+         cout<<"sumB at index "<<i<<": "<<cc.ss[0][i]+cc.ss[1][i]<<endl;
+        // //cout<<"sumS at index "<<i<<": "<<cc.bs[0][i]+cc.bs[1][i]<<endl;
+        // long zz = computeCommitment(g,0,h,r,p);
+        // cout<<"commitment to zero: "<<zz<<endl;
+    }
+
+
+    return true;
+}
+
 int main () {
     default_r1cs_gg_ppzksnark_pp::init_public_params();
     test_r1cs_gg_ppzksnark<default_r1cs_gg_ppzksnark_pp>(1000, 100);
@@ -273,6 +499,24 @@ int main () {
     f.encrypt(text,enc_out);
     f.decrypt(enc_out,dec_out);
     cout<<dec_out<<endl;
+
+    char* id = "127.0.0.1";
+    wallet w = regWallet(id);
+
+    cout<<w.balance<<", "<<w.id<<endl;
+    bool r = signWallet(w);
+    cout<<"signwallet: "<<r<<endl;
+
+    //long commitment = Pedersen(queries);
+
+    commitments c = Pedersen(queries);
+    bool r2 = verifyPederson(c,queries[0],0);
+    cout<<r2<<endl;
+
+    bool r3 = verifyPederson(c,queries[1],1);
+    cout<<r3<<endl;
+    //Sever knows:
+
 
     return 0;
 }
